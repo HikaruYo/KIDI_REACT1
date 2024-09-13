9fc6811b8dd31bc59108fa239ce19ffde50b5d87
HikaruYo <adithaean@gmail.com>
2024-09-13 20:04:25 +0700

    Functional Components

---
+++ src/components/hello.js
@@ -0,0 +1,11 @@
+// Functional Components
+import React from "react";
+
+// function Hello() {
+//     return <h1>Hello World</h1>
+// }
+
+// Kita juga bisa menggunakan Arrow Function dalam Functional Components
+const Hello = () => <h1>Hello World</h1>
+
+export default Hello
