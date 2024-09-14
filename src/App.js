6005c39becdf816fee09f56eb005f18e94f105eb
HikaruYo <adithaean@gmail.com>
2024-09-14 12:23:15 +0700

    Class Components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // import Hello from './components/hello';
 import World from './components/hello';
 // Pada bagian import, kita bisa custom namanya sesuai keinginan
+import Welcome from './components/welcome';
 
 function App() {
   return (
     <div className="App">
       <World></World>
+      <Welcome></Welcome>
     </div>
   );
 }
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+// Class Components
+import React, { Component } from "react";
+
+class Welcome extends Component {
+    render() {
+        return <h3>Welcome Hikaru</h3>
+    }
+}
+
+export default Welcome
